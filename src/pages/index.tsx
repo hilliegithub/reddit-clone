5c591c8d9935f104cfd9de6161d8016ee9f4b83a
Hylton <goregehylton@yahoo.com>
2023-04-09 15:41:29 -0500

    Creating HomePageStructure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import PageContent from "@/Components/Layouts/PageContent";
 
 export default function Home() {
   return (
-    <>
-     
-    </>
-  )
+    <PageContent>
+      <></>
+      <></>
+    </PageContent>
+  );
 }
